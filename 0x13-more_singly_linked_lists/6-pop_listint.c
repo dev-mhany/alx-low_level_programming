5c4b9a6fa53e0eb715ad9e58137e35ee890cf255
Muhammad Hany <dev.mhany@gmail.com>
2023-10-23 12:57:45 -0700

    0x13-more_singly_linked_lists Done .

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -1,22 +1,22 @@
 #include "lists.h"
+#include <stdlib.h>
 
 /**
- * free_listint2 - frees a linked list
- * @head: pointer to the listint_t list to be freed
+ * pop_listint - deletes the head node of a listint_t linked list, and returns
+ * the head node's data (n)
+ * @head: double pointer to the head of the list
+ * Return: the head node's data (n)
  */
-void free_listint2(listint_t **head)
+int pop_listint(listint_t **head)
 {
-	listint_t *temp;
+	listint_t *tmp;
+	int n;
 
-	if (head == NULL)
-		return;
-
-	while (*head)
-	{
-		temp = (*head)->next;
-		free(*head);
-		*head = temp;
-	}
-
-	*head = NULL;
-}
+	if (*head == NULL)
+		return (0);
+	tmp = *head;
+	*head = (*head)->next;
+	n = tmp->n;
+	free(tmp);
+	return (n);
+}}
